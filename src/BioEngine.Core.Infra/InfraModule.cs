b8af077ad0f97838f132e6658673a258ea76e90b
George <sonicgd@gmail.com>
2019-03-13 11:20:26 +0500

    Rework modules to use without host builder

--- src/BioEngine.Core.Infra/InfraModule.cs
+++ src/BioEngine.Core.Infra/InfraModule.cs
@@ -2,22 +2,23 @@ using System;
 using System.Text;
 using BioEngine.Core.Modules;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.AspNetCore;
 using Serilog.Core;
 using Serilog.Events;
 using Serilog.Sinks.Graylog;
-using Serilog.Sinks.Graylog.Core;
 
 namespace BioEngine.Core.Infra
 {
     public class InfraModule : BioEngineModule<InfraModuleConfig>
     {
         private readonly LogLevelController _controller = new LogLevelController();
-        
+
         public override void ConfigureHostBuilder(IWebHostBuilder hostBuilder)
         {
-            AddLogging(hostBuilder);
             hostBuilder.ConfigureServices(services =>
             {
                 services.AddHttpContextAccessor();
@@ -25,50 +26,47 @@ namespace BioEngine.Core.Infra
             });
         }
 
-        private void AddLogging(IWebHostBuilder hostBuilder)
+        public override void ConfigureServices(IServiceCollection services, IConfiguration configuration,
+            IHostingEnvironment environment)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            
-            hostBuilder.ConfigureServices((context, services) =>
+            base.ConfigureServices(services, configuration, environment);
+
+            var isGraylogDisabled =
+                !string.IsNullOrEmpty(configuration["BRC_GRAYLOG_DISABLED"]) &&
+                configuration["BRC_GRAYLOG_DISABLED"] == "true";
+            var loggerConfiguration =
+                new LoggerConfiguration().Enrich.FromLogContext();
+            if (environment.IsDevelopment() || isGraylogDisabled)
             {
-                var isGraylogDisabled =
-                    !string.IsNullOrEmpty(context.Configuration["BRC_GRAYLOG_DISABLED"]) &&
-                    context.Configuration["BRC_GRAYLOG_DISABLED"] == "true";
-                var loggerConfiguration =
-                    new LoggerConfiguration().Enrich.FromLogContext();
-                if (context.HostingEnvironment.IsDevelopment() || isGraylogDisabled)
+                loggerConfiguration = loggerConfiguration
+                    .WriteTo.Console();
+                _controller.Switch.MinimumLevel = Config.DevLevel;
+            }
+            else
+            {
+                var facility = environment.ApplicationName;
+                if (!string.IsNullOrEmpty(configuration["BRC_GRAYLOG_FACILITY"]))
                 {
-                    loggerConfiguration = loggerConfiguration
-                        .WriteTo.LiterateConsole();
-                    _controller.Switch.MinimumLevel = Config.DevLevel;
+                    facility = configuration["BRC_GRAYLOG_FACILITY"];
                 }
-                else
-                {
-                    var facility = context.HostingEnvironment.ApplicationName;
-                    if (!string.IsNullOrEmpty(context.Configuration["BRC_GRAYLOG_FACILITY"]))
+
+                loggerConfiguration = loggerConfiguration
+                    .WriteTo.Graylog(new GraylogSinkOptions
                     {
-                        facility = context.Configuration["BRC_GRAYLOG_FACILITY"];
-                    }
+                        HostnameOrAddress = configuration["BRC_GRAYLOG_HOST"],
+                        Port = int.Parse(configuration["BRC_GRAYLOG_PORT"]),
+                        Facility = facility
+                    });
+                _controller.Switch.MinimumLevel = Config.ProdLevel;
+            }
 
-                    loggerConfiguration = loggerConfiguration
-                        .WriteTo.Graylog(new GraylogSinkOptions
-                        {
-                            HostnameOrAddress = context.Configuration["BRC_GRAYLOG_HOST"],
-                            Port = int.Parse(context.Configuration["BRC_GRAYLOG_PORT"]),
-                            Facility = facility
-                        });
-                    _controller.Switch.MinimumLevel = Config.ProdLevel;
-                }
+            loggerConfiguration.MinimumLevel.ControlledBy(_controller.Switch);
+            Log.Logger = loggerConfiguration.CreateLogger();
 
-                loggerConfiguration.MinimumLevel.ControlledBy(_controller.Switch);
-                Log.Logger = loggerConfiguration.CreateLogger();
-            });
-            hostBuilder.UseSerilog();
-            hostBuilder.ConfigureServices(services =>
-            {
-                services.AddSingleton(_controller);
-                services.AddMvc().AddApplicationPart(typeof(WebHostBuilderExtensions).Assembly);
-            });
+            services.AddSingleton(_controller);
+            services.AddMvc().AddApplicationPart(typeof(WebHostBuilderExtensions).Assembly);
+            services.AddSingleton(_ => (ILoggerFactory)new SerilogLoggerFactory());
         }
     }
 
